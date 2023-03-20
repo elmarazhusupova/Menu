c6de4884976483346b465790cbd82adacc5d462a
elmarazhusupova <zhusupovaelmara80@gmail.com>
2023-03-20 20:59:46 +0600

    add postgres

--- config/settings.py
+++ config/settings.py
@@ -89,8 +89,9 @@ DATABASES = {
     }
 }
 
-db = dj_database_url.config()
+db = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db)
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
