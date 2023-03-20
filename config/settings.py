cc0979913181645f3e269f8206d87cd6745c40df
elmarazhusupova <zhusupovaelmara80@gmail.com>
2023-03-20 21:09:13 +0600

    change postgres to sqlite

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
